0ce258a17071c211a8d159372aa26cf0bc598d66
Bogolepov69 <168251335+Bogolepov69@users.noreply.github.com>
2024-08-25 09:05:58 +0300

    Add files via upload

---
+++ homework_2.py
@@ -0,0 +1,43 @@
+import unittest
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner("Тестовый бегун")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("Тестовый бегун")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Бегун 1")
+        runner2 = Runner("Бегун 2")
+
+        for _ in range(10):
+            runner1.run()  # 10 раз по 10 = 100
+            runner2.walk()  # 10 раз по 5 = 50
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
